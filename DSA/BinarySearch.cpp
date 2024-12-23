258288c167ad52f350175f7cc753663dee9e5aa1
Aryan Revankar <aryanrevankar311@gmail.com>
2024-12-23 22:53:19 +0530

    changes made

--- DSA/BinarySearch.cpp
+++ DSA/BinarySearch.cpp
@@ -2,57 +2,57 @@
 #include<vector>
 using namespace std;
 
-// int BinarySearch(vector<int> arr ,int target) { //itteration
-//     int st = 0;
-//     int end = arr.size() - 1;
+int BinarySearch(vector<int> arr ,int target) { //itteration
+    int st = 0;
+    int end = arr.size() - 1;
     
-//     while(st <= end){
-//         int mid = st + (end - st) / 2;
+    while(st <= end){
+        int mid = st + (end - st) / 2;
 
-//         if (target > arr[mid]){
-//             st = mid + 1;   
-//         }else if(target < arr[mid]){
-//             end = mid - 1;
-//         }else else{
-//             return mid;
-//         }
-//     }    
+        if (target > arr[mid]){
+            st = mid + 1;   
+        }else if(target < arr[mid]){
+            end = mid - 1;
+        }else{
+            return mid;
+        }
+    }    
 
-//     return -1;     
-// }
-
-int recBinarySearch(vector<int> arr, int tar, int st, int end){
-    if(st <= end){
-        int mid = st + (end - st)/2;
-
-    if(tar > arr[mid]){
-        return BinarySearch(arr,tar,mid+1,end);
-    }else if(tar < arr[mid]){
-        return BinarySearch(arr,tar,st,mid-1);
-    }else{
-        return mid;
-    }
-  }
+    return -1;     
 }
+
+// int recBinarySearch(vector<int> arr, int tar, int st, int end){
+//     if(st <= end){
+//         int mid = st + (end - st)/2;
+
+//     if(tar > arr[mid]){
+//         return BinarySearch(arr,tar,mid+1,end);
+//     }else if(tar < arr[mid]){
+//         return BinarySearch(arr,tar,st,mid-1);
+//     }else{
+//         return mid;
+//     }
+//   }
+// }
 
 int main(){         //array needs to be sorted
     vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12};//(odd) //working principle : reduce search space
-    int target = 12;
+    int target = 4;
 
-    int ans = recBinarySearch(arr1,target,arr.size());
-    cout << "Index of "<< target << " is " << ans << endl;
+    // int ans = recBinarySearch(arr1,target,arr.size());
+    // cout << "Index of "<< target << " is " << ans << endl;
 
-    // int ans = BinarySearch(arr1 , target);
-    // cout <<"Index of "<< target << " is " << ans << endl;
+    int ans = BinarySearch(arr1 , target);
+    cout <<"Index of "<< target << " is " << ans << endl;
 
     vector<int> arr2 = {-1, 0, 3, 5, 9, 12};//(even)
-    int target1 = 0 ;      
+    int target1 = 5 ;      
 
-    int ans1 = recBinarySearch(arr2,target1,arr2.size());
-    cout << "Index of "<< target1 << " is " << ans1 << endl;
+    // int ans1 = recBinarySearch(arr2,target1,arr2.size());
+    // cout << "Index of "<< target1 << " is " << ans1 << endl;
 
-    // int ans1 = BinarySearch(arr2 , target1);
-    // cout << "Index of "<< target1 << " is " << ans1 << endl; 
+    int ans1 = BinarySearch(arr2 , target1);
+    cout << "Index of "<< target1 << " is " << ans1 << endl; 
 
     return 0;
 }
